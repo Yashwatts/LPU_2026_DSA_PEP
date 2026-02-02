c31d70703d2dcab9e8546b7ae7049eab9869c4ac
Yash <yashwatts2005@gmail.com>
2026-02-02 16:52:46 +0530

    Started Linked List, insertion at end

---
+++ DSA/05_Linked_List/01.cpp
@@ -0,0 +1,44 @@
+// Singly Linked List is uni-directional
+
+// Doubly Linked List is bi-directional
+
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtEnd(Node* &head, int val){
+    Node* node = new Node(val);
+
+    if(head == NULL){
+        head = node;
+        return;
+    }
+
+    Node* temp = head;
+
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = node;
+}
+
+int main(){
+    // Node* n1 = new Node(1);
+    // Node* head = n1;
+    // Node* n2 = new Node(2);
+    // n1->next = n2;
+
+    Node* head = NULL;
+    insertAtEnd(head, 1);
+
+}
