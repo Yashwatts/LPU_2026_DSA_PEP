52fac7d1257104eb19b2f9f53540709750fef558
Yash <yashwatts2005@gmail.com>
2026-02-03 10:44:03 +0530

    Added insertions in Linked List

--- DSA/05_Linked_List/01.cpp
+++ DSA/05_Linked_List/01.cpp
@@ -32,6 +32,47 @@ void insertAtEnd(Node* &head, int val){
     temp->next = node;
 }
 
+void insertAtBeg(Node* &head, int val){
+    Node* node = new Node(val);
+    
+    if(head == NULL){
+        head = node;
+        return;
+    }
+
+    node -> next = head;
+    head = node;
+}
+
+void insertAtPosition(Node* &head, int pos, int val){
+    Node* node = new Node(val);
+
+    if(pos == 1){
+        node->next = head;
+        head = node;
+        return;
+    }
+
+    Node* temp = head;
+    for(int i=1; i<pos-1 && temp != NULL; i++){
+        temp = temp->next;
+    }
+    if(temp == NULL){
+        return;
+    }
+    node->next = temp-> next;
+    temp->next = node;
+}
+
+void printList(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
 int main(){
     // Node* n1 = new Node(1);
     // Node* head = n1;
@@ -40,5 +81,8 @@ int main(){
 
     Node* head = NULL;
     insertAtEnd(head, 1);
+    insertAtBeg(head, 4);
+    insertAtPosition(head, 7, 9);
+    printList(head);
 
 }
