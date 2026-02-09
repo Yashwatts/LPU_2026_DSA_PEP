d09631c9e30edf60c9aa7859bbac4e721dbd3714
Yash <yashwatts2005@gmail.com>
2026-02-09 10:53:16 +0530

    Added question in Linked List

--- DSA/05_Linked_List/11.cpp
+++ DSA/05_Linked_List/11.cpp
@@ -61,3 +61,63 @@
 //         return merge(sortedLeft, sortedRight);
 //     }
 // };
+
+
+
+
+// https://leetcode.com/problems/copy-list-with-random-pointer/description
+// /*
+// // Definition for a Node.
+// class Node {
+// public:
+//     int val;
+//     Node* next;
+//     Node* random;
+    
+//     Node(int _val) {
+//         val = _val;
+//         next = NULL;
+//         random = NULL;
+//     }
+// };
+// */
+
+// class Solution {
+// public:
+//     Node* copyRandomList(Node* head) {
+//         // I have to clone nodes and insert them right after the original nodes
+//         Node* curr = head;
+
+//         while(curr != NULL){
+//             Node* dummy = new Node(curr->val);
+//             dummy->next = curr->next;
+//             curr->next = dummy;
+//             curr = dummy->next;
+//         }
+
+//         // Then we have to set the random pointer of the cloned nodes
+//         curr = head;
+//         while(curr != NULL){
+//             if(curr->random != NULL){
+//                 curr->next->random = curr->random->next;
+//             }
+//             curr = curr->next->next;
+//         }
+
+//         // Restore the original list and segregate the cloned list
+//         Node* dummy = new Node(0);
+//         Node* tail = dummy;
+
+//         curr = head;
+//         while(curr != NULL){
+//             Node* copyNode = curr->next;
+//             curr->next = copyNode->next;
+
+//             tail->next = copyNode;
+//             tail = tail->next;
+
+//             curr = curr->next;
+//         }
+//         return dummy->next;
+//     }
+// };
