8a234e25b2f0ca6758de7a98bbe0d3ddd477790d
Yash <yashwatts2005@gmail.com>
2026-02-10 14:38:38 +0530

    Added stack question

--- DSA/06_Stacks/09.cpp
+++ DSA/06_Stacks/09.cpp
@@ -45,6 +45,9 @@
 
 
 // https://leetcode.com/problems/implement-stack-using-queues/description/
+
+// By using 2 queues
+
 // class MyStack {
 // public:
 //     queue<int> q1;
@@ -79,6 +82,51 @@
     
 //     bool empty() {
 //         return q1.empty();
+//     }
+// };
+
+// /**
+//  * Your MyStack object will be instantiated and called as such:
+//  * MyStack* obj = new MyStack();
+//  * obj->push(x);
+//  * int param_2 = obj->pop();
+//  * int param_3 = obj->top();
+//  * bool param_4 = obj->empty();
+//  */
+
+
+
+
+// By using 1 queue
+
+// class MyStack {
+// public:
+//     queue<int> q;
+//     MyStack() {
+        
+//     }
+    
+//     void push(int x) {
+//         q.push(x);
+//         int n = q.size();
+//         for(int i=0; i < n-1; i++){
+//             q.push(q.front());
+//             q.pop();
+//         }
+//     }
+    
+//     int pop() {
+//         int topVal = q.front();
+//         q.pop();
+//         return topVal;
+//     }
+    
+//     int top() {
+//         return q.front();
+//     }
+    
+//     bool empty() {
+//         return q.empty();
 //     }
 // };
 
