4f43e6a7fd416e9111c5041d5572cbbf12898441
Yash <yashwatts2005@gmail.com>
2026-02-10 14:29:03 +0530

    Added stack question

--- DSA/06_Stacks/09.cpp
+++ DSA/06_Stacks/09.cpp
@@ -40,3 +40,53 @@
 //  * int param_3 = obj->top();
 //  * int param_4 = obj->getMin();
 //  */
+
+
+
+
+// https://leetcode.com/problems/implement-stack-using-queues/description/
+// class MyStack {
+// public:
+//     queue<int> q1;
+//     queue<int> q2;
+//     MyStack() {
+        
+//     }
+    
+//     void push(int x) {
+//         // Enqueue in q2
+//         q2.push(x);
+
+//         // Take all elements from q1, pop them and push in q2
+//         while(!q1.empty()){
+//             q2.push(q1.front());
+//             q1.pop();
+//         }
+
+//         // Swap q1 and q2
+//         swap(q1, q2);
+//     }
+    
+//     int pop() {
+//         int topVal = q1.front();
+//         q1.pop();
+//         return topVal;
+//     }
+    
+//     int top() {
+//         return q1.front();
+//     }
+    
+//     bool empty() {
+//         return q1.empty();
+//     }
+// };
+
+// /**
+//  * Your MyStack object will be instantiated and called as such:
+//  * MyStack* obj = new MyStack();
+//  * obj->push(x);
+//  * int param_2 = obj->pop();
+//  * int param_3 = obj->top();
+//  * bool param_4 = obj->empty();
+//  */
