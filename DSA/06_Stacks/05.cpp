09be9a145d8b46325a00b18de9fd3bffa80e57e0
Yash <yashwatts2005@gmail.com>
2026-02-10 10:06:04 +0530

    Added Questions of Stack

--- DSA/06_Stacks/05.cpp
+++ DSA/06_Stacks/05.cpp
@@ -1,8 +1,48 @@
-// Deletion from middle of the stack (Recursively - better complexity than taking extra space and storing there)
+// https://www.naukri.com/code360/problems/delete-middle-element-from-stack_985246?leftPanelTabValue=PROBLEM
+// #include <bits/stdc++.h> 
+// void deleteFromStack(stack<int>&inputStack, int count, int size){
+//    if(inputStack.empty() || count == size / 2){
+//       inputStack.pop();
+//       return;
+//    }
+//    int temp = inputStack.top();
+//    inputStack.pop();
+//    deleteFromStack(inputStack, count + 1, size);
+//    inputStack.push(temp);
+// }
+// void deleteMiddle(stack<int>&inputStack, int N){
+	
+//    // Write your code here
+//    int count = 0;
+//    deleteFromStack(inputStack, count, N);
+// }
 
-#include <iostream>
-using namespace std;
 
-int main(){
 
-}
+// https://leetcode.com/problems/valid-parentheses/
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         // Create a stack
+//         // Iterate on string s and check for opening type of bracket
+//         // If found then push in the stack
+//         // Else compare the top element with the s[i], if same then pop the top
+
+//         stack<char> st;
+//         for(int i=0; i<s.length(); i++){
+//             if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+//                 st.push(s[i]);
+//             }
+//             else{
+//                 if(st.empty()) return false;
+
+//                 char top = st.top();
+//                 if((s[i] == ')' && top == '(') || (s[i] == '}' && top == '{') || (s[i] == ']' && top == '[')){
+//                     st.pop();
+//                 }
+//                 else return false;
+//             }
+//         }
+//         return st.empty();
+//     }
+// };
