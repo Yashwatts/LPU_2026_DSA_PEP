8d1be541c261daea58ca6ae48d075097cdd6df6c
Yash <yashwatts2005@gmail.com>
2026-02-11 09:29:27 +0530

    Added another solution to the question in queue

--- DSA/06_Stacks/09.cpp
+++ DSA/06_Stacks/09.cpp
@@ -223,6 +223,7 @@
 
 // Another Solution
 
+
 // class Solution {
 //   public:
 //     void usingStack(queue<int> &q, stack<int> st1, stack<int> st2){
@@ -251,5 +252,53 @@
 //             q.pop();
 //         }
 //         usingStack(q, st1, st2);
+//     }
+// };
+
+
+// Another Solution
+
+
+// class Solution {
+//   public:
+//     // 11 12 13 14 15 16 17 18 19 20
+//     void rearrangeQueue(queue<int> &q) {
+//         // code here
+//         stack<int> st;
+//         int halfSize = q.size() / 2;
+        
+//         // Push first half elements into the stack
+//         // queue : 16 17 18 19 20 || stack : 15 14 13 12 11
+//         for(int i=0; i<halfSize; i++){
+//             st.push(q.front());
+//             q.pop();
+//         }
+//         // Enqueue back the stack elements
+//         // queue : 16 17 18 19 20 15 14 13 12 11
+//         while(!st.empty()){
+//             q.push(st.top());
+//             st.pop();
+//         }
+//         // dequeue the first half elements of queue and enqueue them back
+//         // queue : 15 14 13 12 11 16 17 18 19 20
+//         for(int i=0; i<halfSize; i++){
+//             q.push(q.front());
+//             q.pop();
+//         }
+        
+//         // Again push the first half elements in stack
+//         // queue : 16 17 18 19 20 || stack : 11 12 13 14 15
+//         for(int i=0; i<halfSize; i++){
+//             st.push(q.front());
+//             q.pop();
+//         }
+//         // Interleave the elements of queue and stack
+//         // queue : 11 16 12 17 13 18 14 19 15 20
+//         while(!st.empty()){
+//             q.push(st.top());
+//             st.pop();
+//             q.push(q.front());
+//             q.pop();
+//         }
 //     }
 // };
