c4840e4fede0507479e81829c608ff3a1adfe314
Yash <yashwatts2005@gmail.com>
2026-02-11 14:54:56 +0530

    Histogram in leetcode

--- DSA/06_Stacks/10.cpp
+++ DSA/06_Stacks/10.cpp
@@ -136,3 +136,58 @@
 //         return ans;
 //     }
 // };
+
+
+
+
+// https://leetcode.com/problems/largest-rectangle-in-histogram/description/
+// class Solution {
+// public:
+//     int largestRectangleArea(vector<int>& heights) {
+//         int n = heights.size();
+//         stack<int> st;
+
+//         vector<int> left(n);
+//         vector<int> right(n);
+
+//         // Find the previous smaller
+//         for(int i = 0; i < n; i++){
+//             while(!st.empty() && heights[st.top()] >= heights[i]){
+//                 st.pop();
+//             }
+//             if(st.empty()){
+//                 left[i] = -1;
+//             }
+//             else{
+//                 left[i] = st.top();
+//             }
+//             st.push(i);
+//         }
+
+//         while(!st.empty()) st.pop();
+
+//         // Find next smaller
+//         for(int i=n-1; i>=0; i--){
+//             while(!st.empty() && heights[st.top()] >= heights[i]){
+//                 st.pop();
+//             }
+//             if(st.empty()){
+//                 right[i] = n;
+//             }
+//             else{
+//                 right[i] = st.top();
+//             }
+//             st.push(i);
+//         }
+        
+//         // Calculate max area
+//         int maxArea = 0;
+//         for(int i=0; i<n; i++){
+//             int height = heights[i];
+//             int width = right[i] - left[i] - 1;
+//             int area = height * width;
+//             maxArea = max(maxArea, area);
+//         }
+//         return maxArea;
+//     }
+// };
