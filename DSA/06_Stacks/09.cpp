ae64e4f558e740246810d611ba50fc23cbea3d5f
Yash <yashwatts2005@gmail.com>
2026-02-11 09:57:39 +0530

    Next Greater Element - Stack

--- DSA/06_Stacks/09.cpp
+++ DSA/06_Stacks/09.cpp
@@ -302,35 +302,3 @@
 //         }
 //     }
 // };
-
-
-
-
-
-
-
-// https://www.geeksforgeeks.org/problems/next-larger-element-1587115620/1
-// class Solution {
-//   public:
-//     vector<int> nextLargerElement(vector<int>& arr) {
-//         // code here
-//         int n = arr.size();
-//         stack<int> st;
-//         vector<int> ans(n);
-        
-//         for(int i=n-1; i>=0; i--){
-//             // Remove all elements smaller than or equal to current element
-//             while(!st.empty() && st.top() <= arr[i]){
-//                 st.pop();
-//             }
-//             if(st.empty()){
-//                 ans[i] = -1;
-//             }
-//             else{
-//                 ans[i] = st.top();
-//             }
-//             st.push(arr[i]);
-//         }
-//         return ans;
-//     }
-// };
