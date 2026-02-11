85f6fed83b3c8602c51fba08d664c3e04230ff32
Yash <yashwatts2005@gmail.com>
2026-02-11 10:29:51 +0530

    Previous Greater Element - Stack

--- DSA/06_Stacks/10.cpp
+++ DSA/06_Stacks/10.cpp
@@ -78,3 +78,31 @@
 //         return ans;
 //     }
 // };
+
+
+
+
+// https://www.geeksforgeeks.org/problems/previous-greater-element/1
+// class Solution {
+//   public:
+//     vector<int> preGreaterEle(vector<int>& arr) {
+//         //  code here
+//         int n = arr.size();
+//         vector<int> ans(n);
+//         stack<int> st;
+        
+//         for(int i=0; i<n; i++){
+//             while(!st.empty() && st.top() <= arr[i]){
+//                 st.pop();
+//             }
+//             if(st.empty()){
+//                 ans[i] = -1;
+//             }
+//             else{
+//                 ans[i] = st.top();
+//             }
+//             st.push(arr[i]);
+//         }
+//         return ans;
+//     }
+// };
