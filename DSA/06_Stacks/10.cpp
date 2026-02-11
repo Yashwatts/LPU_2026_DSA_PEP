2f48c8239efde4449ad192ad439d04f05e9019fb
Yash <yashwatts2005@gmail.com>
2026-02-11 10:17:44 +0530

    Previous Smaller Element - Stack

--- DSA/06_Stacks/10.cpp
+++ DSA/06_Stacks/10.cpp
@@ -23,3 +23,31 @@
 //         return ans;
 //     }
 // };
+
+
+
+
+// https://www.geeksforgeeks.org/problems/previous-smaller-element/1
+// class Solution {
+//   public:
+//     vector<int> prevSmaller(vector<int>& arr) {
+//         //  code here
+//         stack<int> st;
+//         int n = arr.size();
+//         vector<int> ans(n);
+        
+//         for(int i=0; i<n; i++){
+//             while(!st.empty() && st.top() >= arr[i]){
+//                 st.pop();
+//             }
+//             if(st.empty()){
+//                 ans[i] = -1;
+//             }
+//             else{
+//                 ans[i] = st.top();
+//             }
+//             st.push(arr[i]);
+//         }
+//         return ans;
+//     }
+// };
