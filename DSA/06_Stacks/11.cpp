304e4895da20ddbea9ff384dddf6bab59c2b51f7
Yash <yashwatts2005@gmail.com>
2026-02-12 13:29:04 +0530

    Asteroid Collision

--- DSA/06_Stacks/11.cpp
+++ DSA/06_Stacks/11.cpp
@@ -87,3 +87,45 @@
 //         return start;
 //     }
 // };
+
+
+
+
+// https://leetcode.com/problems/asteroid-collision/description/
+// class Solution {
+// public:
+//     vector<int> asteroidCollision(vector<int>& asteroids) {
+//         stack<int> st;
+
+//         for(int curr : asteroids){
+//             bool destroyed = false;
+//             while(!st.empty() && st.top() > 0 && curr < 0){
+//                 int topVal = st.top();
+
+//                 if(abs(topVal) < abs(curr)){
+//                     st.pop();
+//                     continue;
+//                 }
+//                 // If top is greater, curr explode
+//                 else if(abs(topVal) > abs(curr)){
+//                     destroyed = true;
+//                 }
+//                 // If equal, both explode
+//                 else{
+//                     st.pop();
+//                     destroyed = true;
+//                 }
+//                 break;
+//             }
+//             if(!destroyed){
+//                 st.push(curr);
+//             }
+//         }
+//         vector<int> result(st.size());
+//         for(int i=st.size()-1; i>=0; i--){
+//             result[i] = st.top();
+//             st.pop();
+//         }
+//         return result;
+//     }
+// };
