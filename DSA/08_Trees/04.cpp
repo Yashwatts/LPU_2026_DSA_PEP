cd4a982f8438d14d68aceb6fe9abb515aff3ab7c
Yash <yashwatts2005@gmail.com>
2026-02-13 15:42:42 +0530

    Postorder in trees

--- DSA/08_Trees/04.cpp
+++ DSA/08_Trees/04.cpp
@@ -58,3 +58,33 @@
 //         return result;
 //     }
 // };
+
+
+
+// https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     void postorderHelper(TreeNode* root, vector<int> &result){
+//         if(root == NULL) return;
+
+//         postorderHelper(root->left, result);
+//         postorderHelper(root->right, result);
+//         result.push_back(root->val);
+//     }
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> result;
+//         postorderHelper(root, result);
+//         return result;
+//     }
+// };
