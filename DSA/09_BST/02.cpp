7344e7a286ae1c857a0d173a597b41f5682d969a
Yash <yashwatts2005@gmail.com>
2026-02-17 13:44:21 +0530

    Convert Sorted Array to BST

--- DSA/09_BST/02.cpp
+++ DSA/09_BST/02.cpp
@@ -54,3 +54,41 @@
 //         return sum;
 //     }
 // };
+
+
+
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     TreeNode* buildBST(vector<int>& nums, int l, int r){
+//         if(l > r) return NULL;
+//         int mid = l+(r-l) / 2;
+
+//         TreeNode* root = new TreeNode(nums[mid]);
+//         root->left = buildBST(nums, l, mid-1);
+//         root->right = buildBST(nums, mid+1, r);
+
+//         return root;
+//     }
+//     TreeNode* sortedArrayToBST(vector<int>& nums) {
+//         // Find mid element
+//         // Make nums[mid] the curr node
+//         // Recursively build-
+//             // LST -> nums[l] .... mid-1
+//             // RST -> nums[mid+1] ..... r
+//             // return ...
+
+//         return buildBST(nums, 0, nums.size()-1);
+//     }
+// };
