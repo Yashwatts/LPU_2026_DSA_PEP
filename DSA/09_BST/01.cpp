4a935ecb03440ea293d6dcfd84f5f4b16134c149
Yash <yashwatts2005@gmail.com>
2026-02-17 11:23:02 +0530

    Started BST

---
+++ DSA/09_BST/01.cpp
@@ -0,0 +1,10 @@
+/*
+
+A Binary Search Tree(BST) is a special type of tree where
+    # Every node has at most two children
+    # For Every Node -
+        All values in the LST are smaller
+        All values in the RST are greater
+    # Both LST and RST are also BST
+
+*/
