fc10c7e3e34dade02b99ad10323ee234f9fdccbb
Yash <yashwatts2005@gmail.com>
2026-02-17 14:52:00 +0530

    Kth Smallest Element in BST

--- DSA/09_BST/02.cpp
+++ DSA/09_BST/02.cpp
@@ -191,3 +191,65 @@
 //         return false;
 //     }
 // };
+
+
+
+// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     void inorder(TreeNode* root, vector<int>& result){
+//         if(root == NULL) return;
+//         inorder(root->left, result);
+//         result.push_back(root->val);
+//         inorder(root->right, result);
+//     }
+//     int kthSmallest(TreeNode* root, int k) {
+//         vector<int> result;
+//         inorder(root, result);
+
+//         int ksmall = result[k-1];
+//         return ksmall;
+//     }
+// };
+
+
+// Another Answer
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     int count = 0;
+//     int ans = 0;
+//     void inorder(TreeNode* root, int k){
+//         if(root == NULL) return;
+//         inorder(root->left, k);
+//         count++;
+//         if(count == k) ans += root->val;
+//         inorder(root->right, k);
+//     }
+//     int kthSmallest(TreeNode* root, int k) {
+//         inorder(root, k);
+//         return ans;
+//     }
+// };
