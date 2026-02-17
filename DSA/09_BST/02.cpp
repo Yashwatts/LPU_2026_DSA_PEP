735e036ec94f542ccc8780de76fbdb49c014e881
Yash <yashwatts2005@gmail.com>
2026-02-17 16:35:01 +0530

    Validate BST

--- DSA/09_BST/02.cpp
+++ DSA/09_BST/02.cpp
@@ -253,3 +253,38 @@
 //         return ans;
 //     }
 // };
+
+
+
+
+
+// https://leetcode.com/problems/validate-binary-search-tree/
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     bool validate(TreeNode* root, long long min, long long max){
+//         // If node is null
+//         // If node->val <= min OR >= max ->false
+//         // recursively validate:
+//         //      LST(min, node->val);
+//         //      RST(node->val, max);
+//         // If both are true then only return true
+
+//         if(root == NULL) return true;
+//         if(root->val <= min || root->val >= max) return false;
+//         return validate(root->left, min, root->val) && validate(root->right, root->val, max);
+//     }
+//     bool isValidBST(TreeNode* root) {
+//         return validate(root, LONG_MIN, LONG_MAX);
+//     }
+// };
