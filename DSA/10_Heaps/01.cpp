9bd8d6248ac9bf4f56e9bcc04d95059aa8ab6d03
Yash <yashwatts2005@gmail.com>
2026-02-19 13:46:00 +0530

    Started Heaps

---
+++ DSA/10_Heaps/01.cpp
@@ -0,0 +1,25 @@
+/*
+
+Heaps -> A heap is a Complete Binary Tree that follows the heap order property
+
+    - Complete Binary Tree (CBT) -> All levels are completely filled except the last level, the last level is filled from left to right
+    - Heap Order Property:
+        1. Max Heap -> Every parent node is greater than or equal to its children
+                       The largest element is always at root
+        2. Min Heap -> Every parent node is smaller than or equal to its children
+                       The smallest element is always to top
+        
+** Important -> Heap is not a BST
+
+
+For any node,
+0-Based Indexing:
+    -> Left Child -> 2 * (i+1)
+    -> Right Child -> 2 * (i+2)
+    -> Parent -> (i-1) / 2
+
+1-Based Indexing:
+    -> Left Child -> 2 * i
+    -> Right Child -> 2 * (i+1)
+    -> Parent -> i / 2
+*/
