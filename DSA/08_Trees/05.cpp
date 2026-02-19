b374a7bad18a329bfc426b36e25d7bf1df7654b5
Yash <yashwatts2005@gmail.com>
2026-02-19 12:42:15 +0530

    Sum of a Binary Tree

--- DSA/08_Trees/05.cpp
+++ DSA/08_Trees/05.cpp
@@ -24,3 +24,35 @@
 //         return 1 + max(lh, rh);
 //     }
 // };
+
+
+
+
+// https://www.geeksforgeeks.org/problems/sum-of-binary-tree/1
+// /* A binary tree node has data, pointer to left child
+//    and a pointer to right child
+// struct Node
+// {
+//     int data;
+//     struct Node* left;
+//     struct Node* right;
+
+//     Node(int x){
+//         data = x;
+//         left = right = NULL;
+//     }
+// }; */
+
+// // Function to return a list containing the level order traversal in spiral form.
+// class Solution {
+//   public:
+//     int sumBT(Node* root) {
+//         // code here
+//         if(root == NULL) return 0;
+        
+//         int ls = sumBT(root->left);
+//         int rs = sumBT(root->right);
+        
+//         return root->data + ls + rs;
+//     }
+// };
