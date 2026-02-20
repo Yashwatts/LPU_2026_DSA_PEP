a48724d3a81a832e4c6e37206d16dea5b3e9d9d8
Yash <yashwatts2005@gmail.com>
2026-02-20 15:03:15 +0530

    Kth smallest element in array

--- DSA/10_Heaps/05.cpp
+++ DSA/10_Heaps/05.cpp
@@ -13,3 +13,22 @@
 //         return pq_min.top();
 //     }
 // };
+
+
+
+
+// https://www.geeksforgeeks.org/problems/kth-smallest-element5635/1
+// class Solution {
+//   public:
+//     int kthSmallest(vector<int> &arr, int k) {
+//         // code here
+//         priority_queue<int> pq;
+        
+//         for(int i : arr){
+//             pq.push(i);
+            
+//             if(pq.size() > k) pq.pop();
+//         }
+//         return pq.top();
+//     }
+// };
