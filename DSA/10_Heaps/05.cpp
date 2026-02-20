bba9f6583ac4a5dbd7771b98bbbc4d5fb10b9526
Yash <yashwatts2005@gmail.com>
2026-02-20 15:49:50 +0530

    Nearly Sorted

--- DSA/10_Heaps/05.cpp
+++ DSA/10_Heaps/05.cpp
@@ -57,3 +57,33 @@
 //         return ans;
 //     }
 // };
+
+
+
+
+// https://www.geeksforgeeks.org/problems/nearly-sorted-1587115620/1
+// class Solution {
+//   public:
+//     void nearlySorted(vector<int>& arr, int k) {
+//         // code here
+//         priority_queue<int, vector<int>, greater<int>> pq;
+        
+//         int n = arr.size();
+//         for(int i=0; i<k+1; i++){
+//             pq.push(arr[i]);
+//         }
+        
+//         int idx = 0;
+        
+//         for(int i=k+1; i<n; i++){
+//             arr[idx++] = pq.top();
+//             pq.pop();
+//             pq.push(arr[i]);
+//         }
+        
+//         while(!pq.empty()){
+//             arr[idx++] = pq.top();
+//             pq.pop();
+//         }
+//     }
+// };
