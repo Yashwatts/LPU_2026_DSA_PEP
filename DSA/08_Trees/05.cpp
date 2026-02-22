8880869903f0f0a7a820e29cd9e09c05eeb0b050
Yash <yashwatts2005@gmail.com>
2026-02-22 19:12:23 +0530

    Binary Tree Level Order Traversal

--- DSA/08_Trees/05.cpp
+++ DSA/08_Trees/05.cpp
@@ -143,3 +143,45 @@
 //         return root;
 //     }
 // };
+
+
+
+
+// https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     vector<vector<int>> levelOrder(TreeNode* root) {
+//         vector<vector<int>> result;
+//         if(root == NULL) return result;
+//         queue<TreeNode*> q;
+//         q.push(root);
+        
+//         while(!q.empty()){
+//             int levelSize = q.size();
+//             vector<int> levelElements;
+
+//             for(int i=0; i<levelSize; i++){
+//                 TreeNode* frontNode = q.front();
+//                 q.pop();
+
+//                 levelElements.push_back(frontNode->val);
+
+//                 if(frontNode->left) q.push(frontNode->left);
+//                 if(frontNode->right) q.push(frontNode->right);
+//             }
+//             result.push_back(levelElements);
+//         }
+//         return result;
+//     }
+// };
