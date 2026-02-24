d060ab17e32703ea9bfeae63492cec6f752b3c5d
Yash <yashwatts2005@gmail.com>
2026-02-24 15:02:59 +0530

    Detect cycle in an undirected graph using DFS

--- DSA/11_Graphs/06.cpp
+++ DSA/11_Graphs/06.cpp
@@ -53,4 +53,48 @@ class Solution {
     }
 };
 
+
+
+
+2. Using DFS (Recursive)
+    - We take visited array, adjacency list and a map to keep track of child->parent
+    - If I'm visiting the node from a parent and visiting again through different parent then there is cycle
+
+https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
+class Solution {
+  public:
+    bool dfs(int node, vector<vector<int>> &adj, vector<bool> &visited, unordered_map<int, int> &parent){
+        visited[node] = true;
+        
+        for(int neighbour : adj[node]){
+            if(!visited[neighbour]){
+                parent[neighbour] = node;
+                if(dfs(neighbour, adj, visited, parent)) return true;
+            }
+            else if(parent[node] != neighbour) return true;
+        }
+        return false;
+    }
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        // Code here
+        vector<vector<int>> adj(V);
+        for(auto &i : edges){
+            int u = i[0];
+            int v= i[1];
+            
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        vector<bool> visited(V, false);
+        for(int i=0; i<V; i++){
+            if(!visited[i]){
+                unordered_map<int, int> parent;
+                parent[i] = -1;
+                if(dfs(i, adj, visited, parent)) return true;
+            }
+        }
+        return false;
+    }
+};
+
 */
