e2b428e12c46187abafc0a69a894a2903d20f0b9
Yash <yashwatts2005@gmail.com>
2026-02-27 14:47:43 +0530

    Shortest Path in DAG

--- DSA/11_Graphs/08.cpp
+++ DSA/11_Graphs/08.cpp
@@ -47,3 +47,126 @@
 //         return minutes;
 //     }
 // };
+
+
+
+
+// https://www.geeksforgeeks.org/problems/shortest-path-in-undirected-graph/1
+// // User function Template for C++
+// class Solution {
+//   public:
+//     void dfs(int node, vector<vector<pair<int, int>>> &adj, vector<int> &visited, stack<int> &st){
+//         visited[node] = 1;
+        
+//         for(auto &i : adj[node]){
+//             int neighbour = i.first;
+//             if(!visited[neighbour]){
+//                 dfs(neighbour, adj, visited, st);
+//             }
+//         }
+//         // after visited all neighbours, push current node into stack (from here i cannot go further to any neighbour)
+//         st.push(node);
+//     }
+//     vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
+//         // code here
+//         vector<vector<pair<int, int>>> adj(V);
+//         for(int i=0; i<E; i++){
+//             int u = edges[i][0];
+//             int v = edges[i][1];
+//             int w = edges[i][2];
+//             adj[u].push_back({v, w});
+//         }
+//         vector<int> visited(V, 0);
+//         stack<int> st;
+        
+//         for(int i=0; i<V; i++){
+//             if(!visited[i]){
+//                 dfs(i, adj, visited, st);
+//             }
+//         }
+//         vector<int> dist(V, INT_MAX);
+//         dist[0] = 0;
+        
+//         while(!st.empty()){
+//             int node = st.top();
+//             st.pop();
+            
+//             if(dist[node] != INT_MAX){
+//                 for(auto &edge : adj[node]){
+//                     int neighbour = edge.first;
+//                     int weight = edge.second;
+                    
+//                     if(dist[node] + weight < dist[neighbour]){
+//                         dist[neighbour] = dist[node] + weight;
+//                     }
+//                 }
+//             }
+//         }
+//         for(int i=0; i<V; i++){
+//             if(dist[i] == INT_MAX){
+//                 dist[i] = -1;
+//             }
+//         }
+//         return dist;
+//     }
+// };
+
+// OR
+
+// https://www.naukri.com/code360/problems/shortest-path-in-dag_8381897?leftPanelTabValue=PROBLEM
+// void dfs(int node, vector<vector<pair<int, int>>> &adj, vector<int> &visited, stack<int> &st){
+//     visited[node] = 1;
+
+//     for(auto &i : adj[node]){
+//         int neighbour = i.first;
+//         if(!visited[neighbour]){
+//             dfs(neighbour, adj, visited, st);
+//         }
+//     }
+//     // after visited all neighbours, push current node into stack (from here i cannot go further to any neighbour)
+//     st.push(node);
+// }
+
+// vector<int> shortestPathInDAG(int n, int m, vector<vector<int>> &edges)
+// {
+//     // Write your code here
+//     vector<vector<pair<int, int>>> adj(n);
+//     for(int i=0; i<m; i++){
+//         int u = edges[i][0];
+//         int v = edges[i][1];
+//         int w = edges[i][2];
+//         adj[u].push_back({v, w});
+//     }
+//     vector<int> visited(n, 0);
+//     stack<int> st;
+
+//     for(int i=0; i<n; i++){
+//         if(!visited[i]){
+//             dfs(i, adj, visited, st);
+//         }
+//     }
+//     vector<int> dist(n, INT_MAX);
+//     dist[0] = 0;
+
+//     while(!st.empty()){
+//         int node = st.top();
+//         st.pop();
+
+//         if(dist[node] != INT_MAX){
+//             for(auto &edge : adj[node]){
+//                 int neighbour = edge.first;
+//                 int weight = edge.second;
+
+//                 if(dist[node] + weight < dist[neighbour]){
+//                     dist[neighbour] = dist[node] + weight;
+//                 }
+//             }
+//         }
+//     }
+//     for(int i=0; i<n; i++){
+//         if(dist[i] == INT_MAX){
+//             dist[i] = -1;
+//         }
+//     }
+//     return dist;
+// }
